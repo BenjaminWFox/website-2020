9a4665b8f38cbecbfa4eec53c365e47f95051814
Ben Fox <BenjaminWFox@gmail.com>
2021-09-09 22:49:40 -0700

    Add js13k 2021 entries

---
+++ pages/lab/js13k/2021/frontiers/index.js
@@ -0,0 +1,13 @@
+export default function Test() {
+  return (
+    <iframe
+      src="/lab/js13k/2021/frontiers/index.html"
+      style={{
+        width: '100%',
+        height: '100vh',
+        border: 'none',
+        overflow: 'hidden',
+      }}
+    />
+  )
+}
