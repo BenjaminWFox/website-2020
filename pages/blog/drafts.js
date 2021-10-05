7020565b04efaed08c63f21c3070bf07b0203288
Ben Fox <BenjaminWFox@gmail.com>
2021-10-04 20:42:59 -0700

    Fix link path forcing reload on draft navigation

--- pages/blog/drafts.js
+++ pages/blog/drafts.js
@@ -60,7 +60,7 @@ export default function Home({ postData }) {
               <h2>
                 <Link
                   as={`/blog/draft/${post.meta.category}/${post.name}`}
-                  href={`/blog/draft/${post.meta.category}/[slug]`}
+                  href="/blog/[...slug]"
                 ><a>{post.meta.title || post.name}</a>
                 </Link>
               </h2>
